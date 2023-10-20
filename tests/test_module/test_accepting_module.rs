5cea3a325c92ae3ffad09f583806aca7f1b518a8
Dariusz Depta <dariusz@confio.gmbh>
2023-10-20 12:48:58 +0200

    Refactored tests.

--- tests/test_module/test_accepting_module.rs
+++ tests/test_module/test_accepting_module.rs
@@ -8,9 +8,8 @@ fn eq(actual: AppResponse, expected: AppResponse) {
     assert_eq!(actual.data, expected.data);
 }
 
-#[test]
-fn accepting_module_default_works() {
-    let accepting_module: AcceptingModule<Empty, Empty, Empty> = AcceptingModule::default();
+/// Utility function for asserting default outputs returned from accepting module.
+fn assert_results(accepting_module: AcceptingModule<Empty, Empty, Empty>) {
     let app = App::default();
     let mut storage = MockStorage::default();
     eq(
@@ -53,45 +52,11 @@ fn accepting_module_default_works() {
 }
 
 #[test]
+fn accepting_module_default_works() {
+    assert_results(AcceptingModule::default());
+}
+
+#[test]
 fn accepting_module_new_works() {
-    let accepting_module: AcceptingModule<Empty, Empty, Empty> = AcceptingModule::new();
-    let app = App::default();
-    let mut storage = MockStorage::default();
-    eq(
-        AppResponse::default(),
-        accepting_module
-            .execute(
-                app.api(),
-                &mut storage,
-                app.router(),
-                &app.block_info(),
-                Addr::unchecked("sender"),
-                Empty {},
-            )
-            .unwrap(),
-    );
-    assert_eq!(
-        Binary::default(),
-        accepting_module
-            .query(
-                app.api(),
-                &storage,
-                &(*app.wrap()),
-                &app.block_info(),
-                Empty {}
-            )
-            .unwrap()
-    );
-    eq(
-        AppResponse::default(),
-        accepting_module
-            .sudo(
-                app.api(),
-                &mut storage,
-                app.router(),
-                &app.block_info(),
-                Empty {},
-            )
-            .unwrap(),
-    );
+    assert_results(AcceptingModule::new());
 }
