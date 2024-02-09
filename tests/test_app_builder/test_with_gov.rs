cd6c1c22a9f6b834a69adb156cfbf2ac7c64807a
Dariusz Depta <dariusz@confio.gmbh>
2024-02-09 09:09:21 +0100

    Refactoring.

--- tests/test_app_builder/test_with_gov.rs
+++ tests/test_app_builder/test_with_gov.rs
@@ -1,5 +1,5 @@
 use crate::test_app_builder::{MyKeeper, NO_MESSAGE};
-use cosmwasm_std::{Addr, Empty, GovMsg, VoteOption};
+use cosmwasm_std::{Empty, GovMsg, VoteOption};
 use cw_multi_test::{no_init, AppBuilder, Executor, Gov};
 
 type MyGovKeeper = MyKeeper<GovMsg, Empty, Empty>;
@@ -10,18 +10,21 @@ const EXECUTE_MSG: &str = "gov execute called";
 
 #[test]
 fn building_app_with_custom_gov_should_work() {
-    // build custom gov keeper (no query and sudo handling for gov)
+    // build custom governance keeper (no query and sudo handling for gov module)
     let gov_keeper = MyGovKeeper::new(EXECUTE_MSG, NO_MESSAGE, NO_MESSAGE);
 
     // build the application with custom gov keeper
     let app_builder = AppBuilder::default();
     let mut app = app_builder.with_gov(gov_keeper).build(no_init);
 
-    // executing gov message should return an error defined in custom keeper
+    // prepare addresses
+    let sender_addr = app.api().addr_make("sender");
+
+    // executing governance message should return an error defined in custom keeper
     assert_eq!(
         EXECUTE_MSG,
         app.execute(
-            Addr::unchecked("sender"),
+            sender_addr,
             GovMsg::Vote {
                 proposal_id: 1,
                 option: VoteOption::Yes,
