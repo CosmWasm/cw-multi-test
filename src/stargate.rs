f9c88084c05ac6478f4800322f6edf27482f0bb8
Dariusz Depta <dariusz@confio.gmbh>
2024-02-09 11:40:28 +0100

    Updates.

--- src/stargate.rs
+++ src/stargate.rs
@@ -1,7 +1,7 @@
 use crate::{AcceptingModule, FailingModule, Module};
 use cosmwasm_std::{AnyMsg, Empty, GrpcQuery};
 
-/// Interface to module handling stargate messages and queries.
+/// Interface to module handling any messages and queries.
 pub trait Stargate: Module<ExecT = AnyMsg, QueryT = GrpcQuery, SudoT = Empty> {}
 
 /// Always accepting stargate module.
