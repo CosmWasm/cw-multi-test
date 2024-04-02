186cc6a524509e97ffeef35889b335cce987bf5e
Dariusz Depta <dariusz@confio.gmbh>
2024-04-02 16:53:08 +0200

    Added tests.

--- tests/test_transaction_info/test_transaction_index.rs
+++ tests/test_transaction_info/test_transaction_index.rs
@@ -1,5 +1,5 @@
 use crate::test_contracts::counter;
-use cosmwasm_std::Empty;
+use cosmwasm_std::{BlockInfo, Empty, Timestamp};
 use cw_multi_test::{App, Executor};
 
 #[test]
@@ -37,6 +37,37 @@ fn execute_should_increment_transaction_index() {
     app.execute_contract(sender_addr, contract_addr, &Empty {}, &[])
         .unwrap();
     assert_eq!(2, app.transaction_info().index);
+}
+
+#[test]
+fn setting_block_should_reset_transaction_index() {
+    let mut app = App::default();
+    let sender_addr = app.api().addr_make("sender");
+    let code_id = app.store_code(counter::contract());
+    let contract_addr = app
+        .instantiate_contract(
+            code_id,
+            sender_addr.clone(),
+            &Empty {},
+            &[],
+            "counter",
+            None,
+        )
+        .unwrap();
+    assert_eq!(1, app.transaction_info().index);
+    app.execute_contract(sender_addr.clone(), contract_addr.clone(), &Empty {}, &[])
+        .unwrap();
+    assert_eq!(2, app.transaction_info().index);
+    // prepare custom block properties
+    let block = BlockInfo {
+        height: 20,
+        time: Timestamp::from_nanos(1_571_797_419_879_305_544),
+        chain_id: "my-testnet".to_string(),
+    };
+    app.set_block(block);
+    app.execute_contract(sender_addr, contract_addr, &Empty {}, &[])
+        .unwrap();
+    assert_eq!(1, app.transaction_info().index);
 }
 
 #[test]
