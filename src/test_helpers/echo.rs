ceb9267b47f3e5f669c9787875648fb920a3806e
Dariusz Depta <dariusz@confio.gmbh>
2024-10-11 10:39:12 +0200

    Refactored echo test helper.

--- src/test_helpers/echo.rs
+++ src/test_helpers/echo.rs
@@ -1,92 +1,92 @@
-//! Very simple echoing contract which just returns incoming string if any,
-//! but performing sub call of given message to test response.
+//! # Echo contract
 //!
-//! Additionally, it bypasses all events and attributes send to it.
+//! Simple echoing contract which just returns incoming data.
 
 use crate::{Contract, ContractWrapper};
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{
     to_json_binary, Attribute, Binary, CustomMsg, Deps, DepsMut, Empty, Env, Event, MessageInfo,
-    Reply, Response, StdError, SubMsg, SubMsgResponse, SubMsgResult,
+    Reply, Response, StdError, StdResult, SubMsg, SubMsgResponse, SubMsgResult,
 };
 use cw_utils::{parse_execute_response_data, parse_instantiate_response_data};
 use serde::de::DeserializeOwned;
 
-// Choosing a reply id less than ECHO_EXECUTE_BASE_ID indicates an Instantiate message reply by convention.
-// An Execute message reply otherwise.
+/// Base identifier value for message replies.
+///
+/// By convention, choosing a reply identifier less than EXECUTE_REPLY_BASE_ID indicates
+/// an `Instantiate` message reply, otherwise it indicates the `Execute` message reply.
 pub const EXECUTE_REPLY_BASE_ID: u64 = i64::MAX as u64;
 
 #[cw_serde]
 #[derive(Default)]
-pub struct Message<ExecC>
+pub struct InitMessage<C>
 where
-    ExecC: CustomMsg + 'static,
+    C: CustomMsg + 'static,
 {
     pub data: Option<String>,
-    pub sub_msg: Vec<SubMsg<ExecC>>,
-    pub attributes: Vec<Attribute>,
-    pub events: Vec<Event>,
+    pub sub_msg: Option<Vec<SubMsg<C>>>,
 }
 
 #[cw_serde]
 #[derive(Default)]
-pub struct InitMessage<ExecC>
+pub struct ExecMessage<C>
 where
-    ExecC: CustomMsg + 'static,
+    C: CustomMsg + 'static,
 {
     pub data: Option<String>,
-    pub sub_msg: Option<Vec<SubMsg<ExecC>>>,
+    pub sub_msg: Vec<SubMsg<C>>,
+    pub attributes: Vec<Attribute>,
+    pub events: Vec<Event>,
 }
 
-fn instantiate<ExecC>(
+fn instantiate<C>(
     _deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    msg: InitMessage<ExecC>,
-) -> Result<Response<ExecC>, StdError>
+    msg: InitMessage<C>,
+) -> StdResult<Response<C>>
 where
-    ExecC: CustomMsg + 'static,
+    C: CustomMsg + 'static,
 {
-    let mut res = Response::new();
+    let mut response = Response::new();
     if let Some(data) = msg.data {
-        res = res.set_data(data.into_bytes());
+        response = response.set_data(data.into_bytes());
     }
     if let Some(msgs) = msg.sub_msg {
-        res = res.add_submessages(msgs);
+        response = response.add_submessages(msgs);
     }
-    Ok(res)
+    Ok(response)
 }
 
-fn execute<ExecC>(
+fn execute<C>(
     _deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    msg: Message<ExecC>,
-) -> Result<Response<ExecC>, StdError>
+    msg: ExecMessage<C>,
+) -> StdResult<Response<C>>
 where
-    ExecC: CustomMsg + 'static,
+    C: CustomMsg + 'static,
 {
-    let mut resp = Response::new();
-
+    let mut response = Response::new();
     if let Some(data) = msg.data {
-        resp = resp.set_data(data.into_bytes());
+        response = response.set_data(data.into_bytes());
     }
-
-    Ok(resp
+    response = response
         .add_submessages(msg.sub_msg)
         .add_attributes(msg.attributes)
-        .add_events(msg.events))
+        .add_events(msg.events);
+    Ok(response)
 }
 
-fn query(_deps: Deps, _env: Env, msg: Empty) -> Result<Binary, StdError> {
+fn query(_deps: Deps, _env: Env, msg: Empty) -> StdResult<Binary> {
     to_json_binary(&msg)
 }
 
-fn reply<ExecC>(_deps: DepsMut, _env: Env, msg: Reply) -> Result<Response<ExecC>, StdError>
+fn reply<C>(_deps: DepsMut, _env: Env, msg: Reply) -> StdResult<Response<C>>
 where
-    ExecC: CustomMsg + 'static,
+    C: CustomMsg + 'static,
 {
-    let res = Response::new();
+    let response = Response::new();
     #[allow(deprecated)]
     if let Reply {
         id,
@@ -96,39 +96,30 @@ where
         ..
     } = msg
     {
-        // We parse out the WasmMsg::Execute wrapper...
-        // TODO: Handle all of Execute, Instantiate, and BankMsg replies differently.
         let parsed_data = if id < EXECUTE_REPLY_BASE_ID {
+            // parse out the WasmMsg::Execute wrapper for instantiate reply
             parse_instantiate_response_data(data.as_slice())
                 .map_err(|e| StdError::generic_err(e.to_string()))?
                 .data
         } else {
+            // parse out the WasmMsg::Execute wrapper for execute reply
             parse_execute_response_data(data.as_slice())
                 .map_err(|e| StdError::generic_err(e.to_string()))?
                 .data
         };
-
         if let Some(data) = parsed_data {
-            Ok(res.set_data(data))
+            Ok(response.set_data(data))
         } else {
-            Ok(res)
+            Ok(response)
         }
     } else {
-        Ok(res)
+        Ok(response)
     }
 }
 
-pub fn contract() -> Box<dyn Contract<Empty>> {
-    let contract = ContractWrapper::new(execute::<Empty>, instantiate::<Empty>, query)
-        .with_reply(reply::<Empty>);
-    Box::new(contract)
-}
-
-pub fn custom_contract<C>() -> Box<dyn Contract<C>>
+pub fn contract<C>() -> Box<dyn Contract<C>>
 where
     C: CustomMsg + DeserializeOwned + 'static,
 {
-    let contract =
-        ContractWrapper::new(execute::<C>, instantiate::<C>, query).with_reply(reply::<C>);
-    Box::new(contract)
+    Box::new(ContractWrapper::new(execute::<C>, instantiate::<C>, query).with_reply(reply::<C>))
 }
