a68edc7f97ae282660800145e68e418c7f950ad4
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-01-28 15:21:31 +0100

    Additional `AppBuilder` tests (#238)

--- src/app_builder.rs
+++ src/app_builder.rs
@@ -41,7 +41,9 @@ pub type BasicAppBuilder<ExecC, QueryC> = AppBuilder<
     StargateFailing,
 >;
 
-/// Utility to build [App] in stages.
+/// The chain builder.
+///
+/// Utility structure for building a chain in stages.
 /// When particular properties are not explicitly set, then default values are used.
 pub struct AppBuilder<Bank, Api, Storage, Custom, Wasm, Staking, Distr, Ibc, Gov, Stargate> {
     api: Api,
@@ -71,6 +73,18 @@ impl Default
         StargateFailing,
     >
 {
+    /// Creates a chain with default settings.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cw_multi_test::{no_init, AppBuilder};
+    ///
+    /// let app = AppBuilder::default().build(no_init);
+    ///
+    /// let sender_addr = app.api().addr_make("sender");
+    /// assert!(sender_addr.as_str().starts_with("cosmwasm1"));
+    /// ```
     fn default() -> Self {
         Self::new()
     }
@@ -90,7 +104,18 @@ impl
         StargateFailing,
     >
 {
-    /// Creates builder with default components working with empty exec and query messages.
+    /// Creates a builder with default components working with empty exec and query messages.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cw_multi_test::{no_init, AppBuilder};
+    ///
+    /// let app = AppBuilder::new().build(no_init);
+    ///
+    /// let sender_addr = app.api().addr_make("sender");
+    /// assert!(sender_addr.as_str().starts_with("cosmwasm1"));
+    /// ```
     pub fn new() -> Self {
         AppBuilder {
             api: MockApi::default(),
