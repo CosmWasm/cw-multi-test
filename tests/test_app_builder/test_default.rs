3f0f12a104eebdffe696b02a3f21c1977a3285d9
Dariusz Depta <dariusz@confio.gmbh>
2025-01-28 10:18:02 +0100

    Added tests for AppBuilder::build with initialization.

--- tests/test_app_builder/test_default.rs
+++ tests/test_app_builder/test_default.rs
@@ -1,5 +1,7 @@
+use cw_multi_test::IntoAddr;
+
 #[test]
-fn default_initialization_should_work() {
+fn default_should_work() {
     use cw_multi_test::{no_init, AppBuilder};
 
     let app = AppBuilder::default().build(no_init);
@@ -10,7 +12,31 @@ fn default_initialization_should_work() {
 }
 
 #[test]
-fn new_constructor_should_work() {
+fn default_with_initialization_should_work() {
+    use cosmwasm_std::coin;
+    use cw_multi_test::AppBuilder;
+
+    let my_address = "me".into_addr();
+    let my_funds = vec![coin(23, "ATOM"), coin(18, "FLOCK")];
+
+    let app = AppBuilder::default().build(|router, _api, storage| {
+        router
+            .bank
+            .init_balance(storage, &my_address, my_funds)
+            .unwrap();
+    });
+
+    assert_eq!(
+        "23ATOM",
+        app.wrap()
+            .query_balance(my_address, "ATOM")
+            .unwrap()
+            .to_string()
+    );
+}
+
+#[test]
+fn new_should_work() {
     use cw_multi_test::{no_init, AppBuilder};
 
     let app = AppBuilder::new().build(no_init);
