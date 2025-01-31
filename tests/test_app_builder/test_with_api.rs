8650036b58754dc923783691493a6f2346605a1c
Dariusz Depta <dariusz@confio.gmbh>
2025-01-31 09:47:27 +0100

    Refactored existing test.

--- tests/test_app_builder/test_with_api.rs
+++ tests/test_app_builder/test_with_api.rs
@@ -3,27 +3,27 @@ use cosmwasm_std::{Api, CanonicalAddr, HexBinary};
 use cw_multi_test::{no_init, AppBuilder};
 
 #[test]
-fn building_app_with_custom_api_should_work() {
-    // prepare test data
+fn building_app_with_customized_api_should_work() {
+    // Prepare test data.
     let human = "juno1h34lmpywh4upnjdg90cjf4j70aee6z8qqfspugamjp42e4q28kqsksmtyp";
     let hex = "bc6bfd848ebd7819c9a82bf124d65e7f739d08e002601e23bb906aacd40a3d81";
 
-    // create application with custom api that implements
-    // Bech32 address encoding with 'juno' prefix
+    // Create the chain with customized Api that implements
+    // Bech32 address encoding with 'juno' prefix.
     let app = AppBuilder::default()
         .with_api(MockApi::default().with_prefix("juno"))
         .build(no_init);
 
-    // check address validation function
+    // Check the address validation function.
     assert_eq!(human, app.api().addr_validate(human).unwrap().as_str());
 
-    // check if address can be canonicalized
+    // Check if the address can be canonicalized.
     assert_eq!(
         app.api().addr_canonicalize(human).unwrap(),
         CanonicalAddr::from(HexBinary::from_hex(hex).unwrap())
     );
 
-    // check if address can be humanized
+    // Check if the address can be humanized.
     assert_eq!(
         human,
         app.api()
@@ -32,6 +32,6 @@ fn building_app_with_custom_api_should_work() {
             .as_str(),
     );
 
-    // check extension function for creating Bech32 encoded addresses
+    // Check the extension function for creating Bech32 encoded addresses.
     assert_eq!(human, app.api().addr_make("creator").as_str());
 }
