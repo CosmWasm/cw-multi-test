a4d613271a0d555b39032fc54aaea532960f5d50
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-04-17 15:53:46 +0200

    Updates.

--- src/prefixed_storage/typed_prefixed_storage.rs
+++ src/prefixed_storage/typed_prefixed_storage.rs
@@ -1,51 +1,87 @@
-use crate::prefixed_storage::{prefixed, prefixed_read, PrefixedStorage, ReadonlyPrefixedStorage};
-use cosmwasm_std::Storage;
+use crate::prefixed_storage::namespace_helpers::{
+    get_with_prefix, range_with_prefix, remove_with_prefix, set_with_prefix,
+};
+use cosmwasm_std::{Order, Record, Storage};
 use std::marker::PhantomData;
 
 pub trait StoragePrefix {
     const PREFIX: &'static [u8];
 }
 
-pub struct TypedPrefixedStorage<'a, T>(ReadonlyPrefixedStorage<'a>, PhantomData<T>);
+pub struct TypedPrefixedStorage<'a, T> {
+    storage: &'a dyn Storage,
+    prefix: &'static [u8],
+    data: PhantomData<T>,
+}
 
 impl<'a, T: StoragePrefix> TypedPrefixedStorage<'a, T> {
     pub fn new(storage: &'a dyn Storage) -> Self {
-        Self(prefixed_read(storage, T::PREFIX), PhantomData)
+        Self {
+            storage,
+            prefix: T::PREFIX,
+            data: PhantomData,
+        }
     }
 }
 
 impl<T> Storage for TypedPrefixedStorage<'_, T> {
     fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
-        self.0.get(key)
+        get_with_prefix(self.storage, self.prefix, key)
     }
 
-    fn set(&mut self, key: &[u8], value: &[u8]) {
-        self.0.set(key, value)
+    fn range<'b>(
+        &'b self,
+        start: Option<&[u8]>,
+        end: Option<&[u8]>,
+        order: Order,
+    ) -> Box<dyn Iterator<Item = Record> + 'b> {
+        range_with_prefix(self.storage, self.prefix, start, end, order)
     }
 
-    fn remove(&mut self, key: &[u8]) {
-        self.0.remove(key)
+    fn set(&mut self, _key: &[u8], _value: &[u8]) {
+        unimplemented!();
+    }
+
+    fn remove(&mut self, _key: &[u8]) {
+        unimplemented!();
     }
 }
 
-pub struct TypedPrefixedStorageMut<'a, T>(PrefixedStorage<'a>, PhantomData<T>);
+pub struct TypedPrefixedStorageMut<'a, T> {
+    storage: &'a mut dyn Storage,
+    prefix: &'static [u8],
+    data: PhantomData<T>,
+}
 
 impl<'a, T: StoragePrefix> TypedPrefixedStorageMut<'a, T> {
     pub fn new(storage: &'a mut dyn Storage) -> Self {
-        Self(prefixed(storage, T::PREFIX), PhantomData)
+        Self {
+            storage,
+            prefix: T::PREFIX,
+            data: PhantomData,
+        }
     }
 }
 
 impl<T> Storage for TypedPrefixedStorageMut<'_, T> {
     fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
-        self.0.get(key)
+        get_with_prefix(self.storage, self.prefix, key)
+    }
+
+    fn range<'b>(
+        &'b self,
+        start: Option<&[u8]>,
+        end: Option<&[u8]>,
+        order: Order,
+    ) -> Box<dyn Iterator<Item = Record> + 'b> {
+        range_with_prefix(self.storage, self.prefix, start, end, order)
     }
 
     fn set(&mut self, key: &[u8], value: &[u8]) {
-        self.0.set(key, value)
+        set_with_prefix(self.storage, self.prefix, key, value);
     }
 
     fn remove(&mut self, key: &[u8]) {
-        self.0.remove(key)
+        remove_with_prefix(self.storage, self.prefix, key);
     }
 }
